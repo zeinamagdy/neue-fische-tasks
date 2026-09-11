4fe4f58f6ad7777bc5ebdc888444d0903d016584
Zeinab Magdy <zainabmagdy8@gmail.com>
2026-09-11 10:55:48 +0200

    Adding React-form-hook

--- kikis-delivery-service/app/deliveries/DeliveryForm.tsx
+++ kikis-delivery-service/app/deliveries/DeliveryForm.tsx
@@ -1,26 +1,56 @@
 "use client";
-import { useActionState } from "react";
+import { useForm } from "react-hook-form";
 import { addDelivery, ActionState } from "@/app/actions/deliveryActions";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Button } from "@/components/ui/button";
 export default function DeliveryForm() {
-  const [state, formAction, isPending] = useActionState<ActionState, FormData>(
-    addDelivery,
-    null,
-  );
+  type FormValues = {
+    pickup: string;
+    destination: string;
+  };
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormValues>();
+
+  const onsubmit = (data: FormValues) => {
+    console.log(data);
+  };
   return (
     <div className="grid gap-2">
-      <form action={formAction}>
+      <form onSubmit={handleSubmit(onsubmit)}>
         <Label htmlFor="pickup">Pickup</Label>
-        <Input name="pickup" placeholder="pickup" />
+        <Input
+          {...register("pickup", {
+            required: "Pickup destination is require",
+            minLength: {
+              value: 3,
+              message: "pickup  must be at least 2 characters",
+            },
+          })}
+          placeholder="pickup"
+        />
+        {errors.pickup && <p>{errors.pickup.message}</p>}
+
         <Label htmlFor="destination">destination</Label>
-        <Input name="destination" placeholder="destination" />
-        {state?.ok === false && <p className="text-red-500">{state.error}</p>}
+        <Input
+          {...register("destination", {
+            required: "Pickup destination is require",
+            minLength: {
+              value: 3,
+              message: "pickup  must be at least 2 characters",
+            },
+          })}
+          placeholder="destination"
+        />
+        {errors.destination && <p>{errors.destination.message}</p>}
+
         <Button variant="brand" type="submit">
           Create Request
         </Button>
-        <div>{isPending ? "Creating..." : "Create Delivery"}</div>
       </form>
     </div>
   );
